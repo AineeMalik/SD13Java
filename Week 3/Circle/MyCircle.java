5363dee8c4c72bc6fbcaeac129ff8cdc7f67b077
AineeMalik <117207213+AineeMalik@users.noreply.github.com>
2025-05-15 12:38:01 -0400

    Update MyCircle.java

---
+++ Week 3/Circle/MyCircle.java
@@ -0,0 +1,12 @@
+public class MyCircle {
+    private MyPoint center;
+    private int radius;
+
+    public MyCircle()
+    {
+        this.radius = 1;
+        this.center = new MyPoint(0,0);
+    }
+//Complete the java class
+    
+}
